a25c2e0e717c6e2b6f062867c9d1816fdbccd39f
Nichts Hsu <NichtsVonChaos@gmail.com>
2019-05-14 23:41:27 +0800

    complete grid as STL container

--- datastructrue/grid.hpp
+++ datastructrue/grid.hpp
@@ -2,16 +2,40 @@
 #define GRID_H
 #include <iterator>
 #include <initializer_list>
+#include <exception>
+#include <algorithm>
 
 // Declaration
 template<typename T>
 class Grid
 {
 public:
-	typedef class __Grid_Iterator : public std::iterator<std::input_iterator_tag, T>
+	typedef class __Grid_Const_Iterator : public std::iterator<std::forward_iterator_tag, T>
 	{
 	public:
-		__Grid_Iterator(T* p);
+		__Grid_Const_Iterator(T *const p);
+
+		__Grid_Const_Iterator &operator =(const __Grid_Const_Iterator &iter);
+
+		bool operator !=(const __Grid_Const_Iterator &iter) const;
+
+		bool operator ==(const __Grid_Const_Iterator &iter) const;
+
+		__Grid_Const_Iterator &operator ++();
+
+		__Grid_Const_Iterator operator ++(int);
+
+		const T &operator *() const;
+
+	private:
+		T *const _ptr;
+
+	} const_iterator;
+
+	typedef class __Grid_Iterator : public std::iterator<std::forward_iterator_tag, T>
+	{
+	public:
+		__Grid_Iterator(T *p);
 
 		__Grid_Iterator &operator =(const __Grid_Iterator &iter);
 
@@ -25,8 +49,10 @@ public:
 
 		T &operator *();
 
+		operator const_iterator() const;
+
 	private:
-		T* _ptr;
+		T *_ptr;
 	} iterator;
 
 	Grid();
@@ -40,16 +66,27 @@ public:
 
 	void clear();
 
-	T get(size_t row, size_t col) const;
+	T at(size_t row, size_t col) const;
 	T *operator [](size_t index);
+	const T *operator [](size_t index) const;
+
+	T &front();
+	const T &front() const;
+
+	T &back();
+	const T &back() const;
 
 	size_t rows() const;
 	size_t columns() const;
 	size_t size() const;
 	size_t count(const T &value) const;
+	bool empty() const;
 
 	iterator begin();
 	iterator end();
+
+	const_iterator cbegin() const;
+	const_iterator cend() const;
 
 	void resize(size_t rows, size_t cols);
 	void resize(size_t rows, size_t cols, const T &value);
@@ -59,8 +96,16 @@ public:
 	bool operator !=(const Grid<T> &other) const;
 
 	size_t replace(const T &oldValue, const T &newValue);
-	void setRow(size_t index, const T &value);
-	void setColumn(size_t index, const T &value);
+	size_t replace(const T &oldValue, const T &newValue, size_t row1, size_t col1, size_t row2, size_t col2);
+	size_t replace(const T &oldValue, const T &newValue, iterator begin, iterator end);
+	void fillRow(size_t index, const T &value);
+	void fillColumn(size_t index, const T &value);
+	void fill(const T &value);
+	void fill(const T &value, size_t row1, size_t col1, size_t row2, size_t col2);
+	void fill(const T &value, iterator begin, iterator end);
+
+	void reverse(bool top_bottom = true, bool left_right = true);
+	void swap(Grid &other);
 
 private:
 	T *m_grid;
@@ -129,7 +174,7 @@ Grid<T>::Grid(const Grid<T> &other)
 
 	for(size_t i = 0; i < m_rows; i++)
 		for(size_t j = 0; j < m_columns; j++)
-			m_grid[i][j] = other.get(i, j);
+			m_grid[i][j] = other.at(i, j);
 }
 
 template<typename T>
@@ -150,13 +195,13 @@ Grid<T> &Grid<T>::operator =(const Grid<T> &other)
 
 	for(size_t i = 0; i < m_rows; i++)
 		for(size_t j = 0; j < m_columns; j++)
-			m_grid[i][j] = other.get(i, j);
+			m_grid[i][j] = other.at(i, j);
 }
 
 template<typename T>
 void Grid<T>::clear()
 {
-	if(m_grid)
+	if(m_grid != nullptr)
 		delete[] m_grid;
 	m_grid = nullptr;
 	m_rows = 0;
@@ -164,12 +209,13 @@ void Grid<T>::clear()
 }
 
 template<typename T>
-T Grid<T>::get(size_t row, size_t col) const
+T Grid<T>::at(size_t row, size_t col) const
 {
 	if(row < m_rows && col < m_columns)
 		return m_grid[row * m_columns + col];
 	else
-		return T();
+		throw std::out_of_range(std::string("Index out of range : (") + std::to_string(row) + std::string(", ") + std::to_string(col) +
+								std::string(")/(") + std::to_string(m_rows - 1) + std::string(", ") + std::to_string(m_columns - 1) + std::string(")."));
 };
 
 template<typename T>
@@ -178,7 +224,52 @@ T *Grid<T>::operator[](size_t index)
 	if(index < m_rows)
 		return m_grid + index * m_columns;
 	else
-		return nullptr;
+		throw std::out_of_range(std::string("Index out of range : (") + std::to_string(index) + std::string(", ?)/(") + std::to_string(m_rows - 1) + std::string(", ?)."));
+}
+
+template<typename T>
+const T *Grid<T>::operator [](size_t index) const
+{
+	if(index < m_rows)
+		return m_grid + index * m_columns;
+	else
+		throw std::out_of_range(std::string("Index out of range : (") + std::to_string(index) + std::string(", ?)/(") + std::to_string(m_rows - 1) + std::string(", ?)."));
+}
+
+template<typename T>
+T &Grid<T>::front()
+{
+	if(m_grid == nullptr)
+		throw std::out_of_range(std::string("Index out of range : (0, 0)/(null, null)."));
+	else
+		return m_grid[0];
+}
+
+template<typename T>
+const T &Grid<T>::front() const
+{
+	if(m_grid == nullptr)
+		throw std::out_of_range(std::string("Index out of range : (0, 0)/(null, null)"));
+	else
+		return m_grid[0];
+}
+
+template<typename T>
+T &Grid<T>::back()
+{
+	if(m_grid == nullptr)
+		throw std::out_of_range(std::string("Index out of range : (0, 0)/(null, null)"));
+	else
+		return m_grid[m_rows * m_columns - 1];
+}
+
+template<typename T>
+const T &Grid<T>::back() const
+{
+	if(m_grid == nullptr)
+		throw std::out_of_range(std::string("Index out of range : (0, 0)/(null, null)"));
+	else
+		return m_grid[m_rows * m_columns - 1];
 }
 
 template<typename T>
@@ -211,6 +302,12 @@ size_t Grid<T>::count(const T &value) const
 }
 
 template<typename T>
+bool Grid<T>::empty() const
+{
+	return m_grid == nullptr;
+}
+
+template<typename T>
 typename Grid<T>::iterator Grid<T>::begin()
 {
 	return iterator(m_grid);
@@ -219,7 +316,19 @@ typename Grid<T>::iterator Grid<T>::begin()
 template<typename T>
 typename Grid<T>::iterator Grid<T>::end()
 {
-	return iterator(m_grid + size());
+	return iterator(m_grid + m_rows * m_columns);
+}
+
+template<typename T>
+typename Grid<T>::const_iterator Grid<T>::cbegin() const
+{
+	return const_iterator(m_grid);
+}
+
+template<typename T>
+typename Grid<T>::const_iterator Grid<T>::cend() const
+{
+	return const_iterator(m_grid + m_rows * m_columns);
 }
 
 template<typename T>
@@ -287,7 +396,7 @@ bool Grid<T>::operator ==(const Grid<T> &other) const
 
 	for(int i = 0; i < m_rows; i++)
 		for(int j = 0; j < m_columns; j++)
-			if(m_grid[i * m_columns + j] != other.get(i, j))
+			if(m_grid[i * m_columns + j] != other.at(i, j))
 				return false;
 
 	return true;
@@ -314,7 +423,54 @@ size_t Grid<T>::replace(const T &oldValue, const T &newValue)
 }
 
 template<typename T>
-void Grid<T>::setRow(size_t index, const T &value)
+size_t Grid<T>::replace(const T &oldValue, const T &newValue, size_t row1, size_t col1, size_t row2, size_t col2)
+{
+	if(row1 > row2)
+	{
+		row1 ^= row2;
+		row2 ^= row1;
+		row1 ^= row2;
+	}
+
+	if(col1 > col2)
+	{
+		col1 ^= col2;
+		col2 ^= col1;
+		col1 ^= col2;
+	}
+
+	if(row2 >= m_rows || col2 >= m_columns)
+		throw std::out_of_range(std::string("Index out of range : (") + std::to_string(row2) + std::string(", ") + std::to_string(col2) +
+								std::string(")/(") + std::to_string(m_rows - 1) + std::string(", ") + std::to_string(m_columns - 1) + std::string(")."));
+
+	size_t number = 0;
+	for(size_t i = row1; i <= row2; i++)
+		for(size_t j = col1; j < col2; j++)
+			if(m_grid[i * m_columns + j] == oldValue)
+			{
+				m_grid[i * m_columns + j] = newValue;
+				number++;
+			}
+
+	return number;
+}
+
+template<typename T>
+size_t Grid<T>::replace(const T &oldValue, const T &newValue, typename Grid<T>::iterator begin, typename Grid<T>::iterator end)
+{
+	size_t number = 0;
+	for(;begin != end; begin++)
+		if(*begin == oldValue)
+		{
+			*begin = newValue;
+			number++;
+		}
+
+	return number;
+}
+
+template<typename T>
+void Grid<T>::fillRow(size_t index, const T &value)
 {
 	if(index < m_rows)
 		for(int i = index * m_columns; i < (index + 1) * m_columns; i++)
@@ -322,11 +478,105 @@ void Grid<T>::setRow(size_t index, const T &value)
 }
 
 template<typename T>
-void Grid<T>::setColumn(size_t index, const T &value)
+void Grid<T>::fillColumn(size_t index, const T &value)
 {
 	if(index < m_columns)
 		for(int i = index; i < m_rows * m_columns; i += m_columns)
 			m_grid[i] = value;
+}
+
+template<typename T>
+void Grid<T>::fill(const T &value)
+{
+	for(int i = 0; i < m_rows * m_columns; i++)
+		m_grid[i] = value;
+}
+
+template<typename T>
+void Grid<T>::fill(const T &value, size_t row1, size_t col1, size_t row2, size_t col2)
+{
+	if(row1 > row2)
+	{
+		row1 ^= row2;
+		row2 ^= row1;
+		row1 ^= row2;
+	}
+
+	if(col1 > col2)
+	{
+		col1 ^= col2;
+		col2 ^= col1;
+		col1 ^= col2;
+	}
+
+	if(row2 >= m_rows || col2 >= m_columns)
+		throw std::out_of_range(std::string("Index out of range : (") + std::to_string(row2) + std::string(", ") + std::to_string(col2) +
+								std::string(")/(") + std::to_string(m_rows - 1) + std::string(", ") + std::to_string(m_columns - 1) + std::string(")."));
+
+	for(size_t i = row1; i <= row2; i++)
+		for(size_t j = col1; j < col2; j++)
+			m_grid[i * m_columns + j] = value;
+}
+
+template<typename T>
+void Grid<T>::fill(const T &value, typename Grid<T>::iterator begin, typename Grid<T>::iterator end)
+{
+	for(;begin != end; begin++)
+		*begin = value;
+}
+
+template<typename T>
+void Grid<T>::reverse(bool top_bottom, bool left_right)
+{
+	if(m_grid == nullptr)
+		return;
+
+	if(!top_bottom && !left_right)
+		return;
+
+	if(top_bottom && !left_right)
+	{
+		T temp;
+		for(int i = 0; i < m_rows / 2; i++)
+			for(int j = 0; j < m_columns; j++)
+			{
+				temp = m_grid[i * m_columns + j];
+				m_grid[i * m_columns + j] = m_grid[(m_rows - 1 - i) * m_columns + j];
+				m_grid[(m_rows - 1 - i) * m_columns + j] = temp;
+			}
+
+		return;
+	}
+
+	if(!top_bottom && left_right)
+	{
+		for(int i = 0; i < m_rows; i++)
+			std::reverse(m_grid + i * m_columns, m_grid + (i + 1) * m_columns);
+
+		return;
+	}
+
+	if(top_bottom && left_right)
+	{
+		std::reverse(m_grid, m_grid + m_rows * m_columns);
+		return;
+	}
+}
+
+template<typename T>
+void Grid<T>::swap(Grid<T> &other)
+{
+	T *temp = m_grid;
+	m_grid = other.m_grid;
+	other.m_grid = temp;
+
+	m_rows ^= other.m_rows;
+	other.m_rows ^= m_rows;
+	m_rows ^= other.m_rows;
+
+	m_columns ^= other.m_columns;
+	other.m_columns ^= m_columns;
+	m_columns ^= other.m_columns;
 }
 
 template<typename T>
@@ -364,7 +614,7 @@ typename Grid<T>::__Grid_Iterator &Grid<T>::__Grid_Iterator::operator ++()
 template<typename T>
 typename Grid<T>::__Grid_Iterator Grid<T>::__Grid_Iterator::operator ++(int)
 {
-	__Grid_Iterator tmp= *this;
+	__Grid_Iterator tmp = *this;
 	_ptr++;
 	return tmp;
 }
@@ -373,6 +623,59 @@ template<typename T>
 T &Grid<T>::__Grid_Iterator::operator *()
 {
 	return *_ptr;
+}
+
+template<typename T>
+Grid<T>::__Grid_Const_Iterator::__Grid_Const_Iterator(T *const p)
+{
+	_ptr = p;
+}
+
+template<typename T>
+typename Grid<T>::__Grid_Const_Iterator &Grid<T>::__Grid_Const_Iterator::operator =(const typename Grid<T>::__Grid_Const_Iterator &iter)
+{
+	_ptr = iter._ptr;
+	return *this;
+}
+
+template<typename T>
+bool Grid<T>::__Grid_Const_Iterator::operator !=(const typename Grid<T>::__Grid_Const_Iterator &iter) const
+{
+	return _ptr != iter._ptr;
+}
+
+template<typename T>
+bool Grid<T>::__Grid_Const_Iterator::operator ==(const typename Grid<T>::__Grid_Const_Iterator &iter) const
+{
+	return _ptr == iter._ptr;
+}
+
+template<typename T>
+typename Grid<T>::__Grid_Const_Iterator &Grid<T>::__Grid_Const_Iterator::operator ++()
+{
+	_ptr++;
+	return *this;
+}
+
+template<typename T>
+typename Grid<T>::__Grid_Const_Iterator Grid<T>::__Grid_Const_Iterator::operator ++(int)
+{
+	__Grid_Const_Iterator tmp = *this;
+	_ptr++;
+	return tmp;
+}
+
+template<typename T>
+const T &Grid<T>::__Grid_Const_Iterator::operator *() const
+{
+	return *_ptr;
+}
+
+template<typename T>
+Grid<T>::__Grid_Iterator::operator Grid<T>::const_iterator() const
+{
+	Grid<T>::const_iterator citr(_ptr);
+	return citr;
 }
 
 #endif // GRID_H
